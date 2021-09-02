5ce4b70ddb09e30170fc4347f2400ff7b01b89da
Kory Attleson <kory.attleson@gmail.com>
2021-09-02 15:27:06 -0500

    Created urls.py in music app folder. Finished Django Rest API walkthrough

---
+++ music_project/music/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Song
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
